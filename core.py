fb324273614365b39564e40bc1e6e91238720b1e
Andrey Voroshnin <sonic666@gmail.com>
2022-05-26 16:00:26 +0300

    jump

--- core.py
+++ core.py
@@ -7,7 +7,6 @@ from os.path import join, dirname
 from player import Player
 
 
-
 class Core:
     def __init__(self) -> None:
         pygame.init()
@@ -18,33 +17,36 @@ class Core:
 
     def init(self) -> None:
         self.running = True
-        self.dt = 0
         dotenv_path = join(dirname(__file__), 'config.env')
         load_dotenv(dotenv_path)
         self.bg = pygame.image.load(os.getenv('BG'), "Background")
         self.display = pygame.display.set_mode((self.bg.get_width(), self.bg.get_height()), pygame.DOUBLEBUF | pygame.HWACCEL)
         self.bg = self.bg.convert()
-        self.bgX = 0
-        self.bgY = self.bg.get_width()
+        self.first_bg_pos = 0
+        self.second_bg_pos = self.bg.get_width()
         self.fps = int(os.getenv('FPS'))
         self.player = Player(self.load_player())    
-        self.player_group = pygame.sprite.Group(self.player)
+        self.player_group = pygame.sprite.GroupSingle(self.player)
+        self.ground = 266
+        self.jump_height = 366
+
 
     def event(self, event: pygame.event.Event) -> None:
         if event.type == pygame.QUIT:
             self.running = False
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_SPACE:
+                self.player_jump()
 
 
-    def update(self) -> None:
+    def update(self, delta_time) -> None:
         self.move_bg()
-        self.dt = self.clock.tick(self.fps) / 1000
-        self.player_group.update(self.dt)
-        self.clock.tick(self.fps)
+        self.player_group.update(delta_time)
 
 
     def render(self) -> None:
-        self.display.blit(self.bg, (self.bgX,0))
-        self.display.blit(self.bg,(self.bgY,0))
+        self.display.blit(self.bg, (self.first_bg_pos,0))
+        self.display.blit(self.bg, (self.second_bg_pos,0))
         self.player_group.draw(self.display)
         pygame.display.update()
 
@@ -56,20 +58,21 @@ class Core:
     def run(self) -> None:
         self.init()
         while self.running:
+            delta_time = self.clock.tick(self.fps)
             for event in pygame.event.get():
                 self.event(event)
-            self.update()
+            self.update(delta_time)
             self.render()
         self.cleanup()
     
 
     def move_bg(self) -> None:
-        self.bgX -= self.player.player_speed
-        self.bgY -= self.player.player_speed
-        if self.bgX < self.bg.get_width() * -1:
-            self.bgX = self.bg.get_width()
-        if self.bgY < self.bg.get_width() * -1:
-            self.bgY = self.bg.get_width()
+        self.first_bg_pos -= self.player.player_speed
+        self.second_bg_pos -= self.player.player_speed
+        if self.first_bg_pos < self.bg.get_width() * -1:
+            self.first_bg_pos = self.bg.get_width()
+        if self.second_bg_pos < self.bg.get_width() * -1:
+            self.second_bg_pos = self.bg.get_width()
     
 
     def load_player(self) -> List:
@@ -79,3 +82,7 @@ class Core:
             image.set_colorkey('WHITE')
             images.append(image)
         return images
+
+
+    def player_jump(self) -> None:
+        self.player.rect.move_ip([50, 366])
--- player.py
+++ player.py
@@ -1,39 +1,25 @@
-from email.mime import image
 from typing import List
 import pygame
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, player_images: List) -> None:
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.player_pos = pygame.Vector2(50, 266)
         self.images = player_images
         self.image_index = 0
         self.image = player_images[self.image_index]
-        self.image_size = (self.image.get_width(), self.image.get_height())
         self.player_speed = 3.7
-        self.rect = pygame.Rect(self.player_pos, self.image_size)
-        self.animation_time = 0.05
+        self.rect = self.image.get_rect(topleft=self.player_pos)
+        self.animation_time = 30
         self.current_time = 0
+        self.diry = 0
 
 
-    def update_time_dependent(self, dt):
-        """
-        Updates the image of Sprite approximately every 0.1 second.
-
-        Args:
-            dt: Time elapsed between each frame.
-        """
-
-        self.current_time += dt
+    def update(self, delta_time):
+        self.current_time += delta_time
         if self.current_time >= self.animation_time:
             self.current_time = 0
             self.image_index = (self.image_index + 1) % len(self.images)
             self.image = self.images[self.image_index]
 
-        #self.rect.move_ip(self.player_speed,0)
-
-
-    def update(self, dt):
-        # Switch between the two update methods by commenting/uncommenting.
-        self.update_time_dependent(dt)
